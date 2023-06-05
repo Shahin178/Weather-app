5d0d0413551b8a6495240212a08825b7a58339d1
Shahin <banoshahin89@gmail.com>
2023-06-06 05:14:50 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import Tempapp from "./componenet/tempApp";
+
+function App() {
+  return (
+    <div>
+      <Tempapp />
+    </div>
+  );
+}
+
+export default App;
